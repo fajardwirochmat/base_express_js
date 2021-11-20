b74a9503d3a85b7b0cec86b96edf35bde16d26f2
Fajar Dwi Rochmat <94727244+fajardwirochmat@users.noreply.github.com>
2021-11-20 13:41:59 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'your_telegram_token';
+const token = '2140853258:AAF7AzKdd133wzQsRtGHc6KGpvi7sQZT_00';
 const bot = new TelegramBot(token, {polling: true});
 
 
