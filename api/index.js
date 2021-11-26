f09e19ae384416d8281796dacc48b31fa54875e2
Fajar Dwi Rochmat <94727244+fajardwirochmat@users.noreply.github.com>
2021-11-26 20:58:27 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,12 +10,10 @@ const bot = new TelegramBot(token, {polling: true});
 
 let global_msg_id;
 // Main Menu Bot
-bot.onText(/\/start/, (msg) => {
-    global_msg_id = msg.chat.id;
-    bot.sendMessage(
-        global_msg_id,
-        `hello ${msg.chat.first_name}, welcome...\n
-        click /predict`
+bot.onText(/\/Start/, (msg) => {
+    bot.sendMessage(msg.chat.id, `Welcome, ${msg.chat.first_name}`, {
+    "reply_markup": {
+      "keyboard":[["/Assalamualaikum"],["/Bagaimana Kabar Cuaca"]]
     );
 });
 
